e4b9069d04ee6f0708a3b42fd9611443e0c7f047
_doctorproctor_ <80859455+proctorinc@users.noreply.github.com>
2022-05-07 22:11:04 -0700

    Update index.js
    Added html document connection for react native web hosting

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.runApplication("App", {
+  rootTag: document.getElementById("root")
+});
